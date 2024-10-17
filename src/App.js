834841263227a0af16886185ba136b235dab63e9
shubhammaurya295 <studentcollage71119@gmail.com>
2024-10-17 10:35:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {Button} from 'react-bootstrap'
+
+function App() {
+  return (
+    <>
+<button>Nomal button</button>
+<Button>BootStrap button</Button>
+    </>
+  );
+}
+
+export default App;
